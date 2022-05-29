06767b6880cfa18ac730e6cd123a05905cf9872a
zaynabreza <zaynab.reza@hotmail.com>
2022-05-29 20:38:22 +0500

    New Course option, deletion and fixes

--- brilliantPro-Merged/src/app/course/course.component.ts
+++ brilliantPro-Merged/src/app/course/course.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import {CoursesService} from "../services/courses.service";
 import {CoursesInterface} from "../interfaces/coursesInterface";
 import {CourseDBInterface} from "../interfaces/CourseDBInterface";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {CourseDetailsComponent} from "../course-details/course-details.component";
+import {AddCourseComponent} from "../add-course/add-course.component";
+import {CourseLearnerInterface} from "../interfaces/CourseLearnerInterface";
+import {LearnerService} from "../services/learner.service";
+import {LearnerInterface} from "../interfaces/LearnerInterface";
 
 @Component({
   selector: 'app-course',
@@ -11,7 +17,7 @@ import {CourseDBInterface} from "../interfaces/CourseDBInterface";
 export class CourseComponent implements OnInit {
 
   courses:any[]=[]
-  constructor(private coursesService:CoursesService) { }
+  constructor(private coursesService:CoursesService, private modalService: NgbModal, private learnerService:LearnerService) { }
 
   ngOnInit(): void {
     this.getAllFullCourses();
@@ -21,6 +27,39 @@ export class CourseComponent implements OnInit {
     this.coursesService.getAllFullCourses().subscribe((data:CourseDBInterface[])=>
     {
       this.courses=data;
+    })
+  }
+
+  open(course:CourseDBInterface){
+    console.log("opening for ", course.name)
+    const modalRef = this.modalService.open(CourseDetailsComponent);
+    modalRef.componentInstance.course = course;
+    modalRef.componentInstance.image = course.image;
+  }
+
+  new(){
+    const modalRef = this.modalService.open(AddCourseComponent);
+    modalRef.componentInstance.courses=this.courses;
+  }
+
+  delete(c:CourseDBInterface, index:number){
+      this.coursesService.getAllInCourse(c._id).subscribe((data:CourseLearnerInterface[])=>{
+        console.log(data)
+        for (let i=0; i<data[0].learnerDetails.length; i++){
+          this.learnerService.getLearnerInformation(data[0].learnerDetails[i]._id).subscribe((data:LearnerInterface[])=>{
+            let tempLearner=data[0];
+            for (let j=0; j<tempLearner.courses.length; j++){
+              if (tempLearner.courses[j].courseId==c._id){
+                tempLearner.courses.splice(j, 1)
+                this.learnerService.updateLearner(tempLearner).subscribe((data)=>{
+                  console.log(data)
+                  console.log("Course Deleted from Learner")
+                })
+              }
+            }
+        })
+      }
+        this.coursesService.deleteCourse(c).subscribe((data:any)=>{this.courses.splice(index, 1); console.log("course deleted")})
     })
   }
 
