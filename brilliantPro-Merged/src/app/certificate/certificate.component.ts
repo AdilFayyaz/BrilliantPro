1f22448787d17bce7f1baf91bd26763c268beda8
hurriyanasir <82242392+hurriyanasir@users.noreply.github.com>
2022-06-04 15:35:06 +0500

    certificate download requirement done

--- brilliantPro-Merged/src/app/certificate/certificate.component.ts
+++ brilliantPro-Merged/src/app/certificate/certificate.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+declare var require: any;
+
+// import * as jspdf from 'jspdf';
+import jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-certificate',
@@ -25,4 +30,22 @@ export class CertificateComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public captureScreen()  
+  {  
+    var data = document.getElementById('contentToConvert') as HTMLCanvasElement;;  
+    html2canvas(data).then(canvas => {  
+      // Few necessary setting options  
+      var imgWidth = 208;   
+      var pageHeight = 295;    
+      var imgHeight = canvas.height * imgWidth / canvas.width;  
+      var heightLeft = imgHeight;  
+  
+      const contentDataURL = canvas.toDataURL('image/png')  
+      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
+      var position = 0;  
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
+      pdf.save(this.courseName+'Certificate.pdf'); // Generated PDF   
+    });  
+  }  
+  
 }
